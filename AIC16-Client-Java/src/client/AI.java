1a2be21b06ec0c44f3795655d47c0c59a763f996
ilaimihar <ilaimihar@gmail.com>
2016-02-14 11:54:38 +0330

    bechehaBirizin Incompelete Add Methods requirments for bechehaBirizin in NodeDetails , CGP

--- AIC16-Client-Java/src/client/AI.java
+++ AIC16-Client-Java/src/client/AI.java
@@ -1,5 +1,6 @@
 package client;
 
+import client.model.Graph;
 import client.model.Node;
 import client.score.ScoreHolder;
 import client.score.ScoreSystem;
@@ -7,6 +8,7 @@ import client.utils.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.TreeSet;
 
 /**
  * AI class.
@@ -22,12 +24,17 @@ public class AI {
 
     private boolean debug = true;
     private final String TAG = "AIClass";
+    private Graph lastState = null;
 
     public void doTurn(World world) {
         // fill this method, we've presented a stupid AI for example!
         APSP.initialize(world);
         CGP.initialize(world);
         ScoreSystem.initialize(world);
+
+
+        if (lastState == null) // if its first turn
+            lastState = world.getMap();
 
         Node[] myNodes = world.getMyNodes();
         Logger.log(TAG, world.getMyNodes().length + " tedade node haye man", debug);
@@ -39,15 +46,16 @@ public class AI {
         Arrays.fill(isImportantJobAssignedToNode, false);
         Arrays.fill(isAPathAssignedToNode, false);
 
-        for (Node source : myNodes) {
+//        bechehaBirizin(world, lastState, isImportantJobAssignedToNode);
 
+        for (Node source : myNodes) {
 
 
             if (isImportantJobAssignedToNode[source.getIndex()])
                 continue;
 
             if (!NodeDetails.isAllNeighboursMine(source)) { //bfs to the best node
-                Path.bfsToTheBestNode(world,source);
+                Path.bfsToTheBestNode(world, source);
                 continue;
             }
 
@@ -72,6 +80,44 @@ public class AI {
             }
         }
 
+//        saving the last state for next turn
+        lastState = new Graph(world.getMap().getNodes().clone());
+
+    }
+
+    /**
+     * YA ABALFAZL
+     * ALLAHO AKBAR
+     * this is a ENTEHARI OPERATION
+     */
+    public void bechehaBirizin(World world, Graph lastState, boolean[] isImportantJobAssigned) {
+
+        ArrayList<Node> nearbyEnemies = CGP.getEnemyFrontLiners(); // Fuck'em
+
+        for (Node enemyNode : nearbyEnemies) {
+
+            boolean isEnemyMoving = NodeDetails.isEnemyMoving(world, lastState, enemyNode);
+            boolean amIBetterCombinedToAttack = NodeDetails.amIBetterCombinedToAttack(world, enemyNode);
+
+            if (amIBetterCombinedToAttack && !isEnemyMoving) { //Fuck'em Right Now
+                for (Node node : enemyNode.getNeighbours()) {
+                    if (NodeDetails.isMyNode(world, node)) {
+                        isImportantJobAssigned[node.getIndex()] = true;
+                        world.moveArmy(node, enemyNode, node.getArmyCount());
+                    }
+                }
+            } else if (amIBetterCombinedToAttack && isEnemyMoving) {
+                TreeSet<Node> myNearbyNodes = NodeDetails.getSortedNearbyMyNodes(world, enemyNode);
+                world.moveArmy(myNearbyNodes.first(), enemyNode, myNearbyNodes.first().getArmyCount());
+                isImportantJobAssigned[myNearbyNodes.first().getIndex()] = true;
+                for (Node node : myNearbyNodes.last().getNeighbours()) { //TODO maybe bad algorithm
+                    if(!NodeDetails.isMyNode(world,node))
+                        continue;
+                    isImportantJobAssigned[node.getIndex()] =true;
+                    world.moveArmy(node,myNearbyNodes.first(),node.getArmyCount());
+                }
+            }
+        }
     }
 
 }
--- AIC16-Client-Java/src/client/utils/NodeDetails.java
+++ AIC16-Client-Java/src/client/utils/NodeDetails.java
@@ -1,7 +1,12 @@
 package client.utils;
 
 import client.World;
+import client.model.Graph;
 import client.model.Node;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.TreeSet;
 
 /**
  * Created by al on 2/13/16.
@@ -36,7 +41,7 @@ public class NodeDetails {
         return CGP.isNodeBorder(node);
     }
 
-    public static boolean isAllNeighboursMine(Node node){
+    public static boolean isAllNeighboursMine(Node node) {
         return isAllNeighboursSameAsOwner(node);
     }
 
@@ -45,6 +50,7 @@ public class NodeDetails {
      * works for both my node and enemy node
      * this means if its my node and a enemy node is nearby returns true
      * and if it is enemies node and at least one of my node is nearby returns true
+     *
      * @return a boolean value which tells if a enemy of node source is nearby
      */
     public static boolean isEnemyNearby(World world, Node node) {
@@ -55,5 +61,72 @@ public class NodeDetails {
                 return true;
         }
         return false;
+    }
+
+    /**
+     * compute if enemy is moving from a node
+     */
+    public static boolean isEnemyMoving(World currentWorld, Graph lastState, Node enemyNode) {
+
+        int lastArmyLevel = ArmyLevel.ComputeArmyLevel(currentWorld,
+                lastState.getNode(enemyNode.getIndex())).ordinal();
+
+        int currentArmyLevel = ArmyLevel.ComputeArmyLevel(currentWorld, enemyNode).ordinal();
+
+        return lastArmyLevel > currentArmyLevel;
+
+    }
+
+    /**
+     * compute last army level of a node
+     */
+    public static ArmyLevel.ArmyLevelEnum lastArmyLevel(World lastWorld, Node node) {
+        return ArmyLevel.ComputeArmyLevel(lastWorld, lastWorld.getMap().getNode(node.getIndex()));
+    }
+
+    public static int getCombinedArmyCount(World world, Node enemyNode) {
+
+        int count = 0;
+        for (Node node : enemyNode.getNeighbours()) {
+
+            if (isMyNode(world, node)) {
+                count += node.getArmyCount();
+            }
+        }
+        return count;
+    }
+
+    public static boolean amIBetterThan(World world, Node myNode, Node enemyNode) {
+        int myArmyLevel = ArmyLevel.ComputeArmyLevel(world, myNode).ordinal();
+        int enemyArmyLevel = ArmyLevel.ComputeArmyLevel(world, enemyNode).ordinal();
+        if (myArmyLevel > enemyArmyLevel)
+            return true;
+        else if (myArmyLevel == enemyArmyLevel
+                && myArmyLevel == ArmyLevel.ArmyLevelEnum.High.ordinal()
+                && myNode.getArmyCount() > 85)
+            return true;
+        return false;
+    }
+
+
+    public static boolean amIBetterCombinedToAttack(World world, Node enemyNode) {
+        Node tmpNode = new Node(85);
+        tmpNode.setArmyCount(getCombinedArmyCount(world, enemyNode));
+        return amIBetterThan(world, tmpNode, enemyNode);
+
+    }
+
+    public static TreeSet<Node> getSortedNearbyMyNodes(World world, Node enemyNode) {
+
+
+        TreeSet<Node> nodeTreeSet = new TreeSet<>(
+                (Comparator<Node>) (o1, o2) -> Integer.compare(o1.getArmyCount(), o2.getArmyCount())
+        );
+
+        for (Node node : enemyNode.getNeighbours())
+            if (isMyNode(world, node))
+                nodeTreeSet.add(node);
+
+        return nodeTreeSet;
     }
 }
